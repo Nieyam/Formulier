2daccc5562dd3d699b064b4fa7358eada2d7a029
Nieyam <nieyambusiness@gmail.com>
2020-06-10 17:43:12 +0200

    Make:Form -> ContactForm aangemaakt en velden

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,41 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="contact")
+     * @Route("/", name="contact")
+     * @param Request $request
+     * @param \Swift_Mailer $mailer
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $contactFormData = $form->getData();
+
+            $message = (new \Swift_Message($contactFormData['onderwerp']))
+                ->setFrom('nadia782022@gmail.com')
+                ->setTo($contactFormData['email'])
+                ->setBody(
+                    $contactFormData['bericht'],
+                    'text/plain'
+                );
+            $mailer->send($message);
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView()
         ]);
+
     }
 }
